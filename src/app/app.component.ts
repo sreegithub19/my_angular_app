1ae8d4ce5d8ea318b3b17bd39c618fbdd01c03f7
K <sreedhar.k@rakuten.com>
2022-08-29 23:29:25 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+ count = 0;
+  constructor(
+    private _renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document: Document
+  ) {}
+
+  ngOnInit() {
+    
+    this.loadScriptByAngularWay(
+      'alert("Dynamic loaded SSR")',
+      console.log('hi')
+    );
+  }
+
+   
+  public loadScriptByAngularWay(text,afterload) {
+    let script = this._renderer2.createElement('script');
+    script.type = `text/javascript`;
+    script.text = text;
+
+    this._renderer2.appendChild(this._document.body, script);
+    script.addEventListener('load', () => {
+      afterload(script);
+    });
+  }
 }
