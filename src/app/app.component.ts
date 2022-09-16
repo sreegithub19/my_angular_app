b4d2fef9311a1095db72330a19912f9f8c473a0a
K <sreedhar.k@rakuten.com>
2022-09-17 01:45:47 +0530

    Added changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,29 @@
-import { Component, AfterViewInit, Input, OnInit,Renderer2, Inject } from '@angular/core';
-//require('../using-require-syntax.js');
-//import '../using-import-syntax.js';
-import { DOCUMENT } from '@angular/common';
-import {Injectable } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { get } from "scriptjs";
 
 @Component({
-  selector: 'app-root',
-  template: ``,
-  //styles: [`h1 { font-family: Lato; }`]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    get("", () => {
+      this.initMap();
+    });
+  }
 
+  initMap() {
+    document.getElementById("clicked").addEventListener("click", function () {
+      document.write(`
+          hi clicked
+      `);
+      });
 
-@Injectable()
-export class AppComponent {
-  constructor(@Inject(DOCUMENT) private document: any) {
-      document.write("hi");   // Failed to execute 'write' on 'Document': It isn't possible to write into a document from an asynchronously-loaded external script unless it is explicitly opened.
-      document.getElementById("root").innerHTML =`<h1 onclick = "document.write(\`
-      hi document
-      hello there
-      <h1>hi \\\`there\\\`</h1>
-      <script>alert('hi')</script>
-      <script>alert('hi again')</script>
-      <script>document.write('hi again')</script>
-      \`)">hi</h1>
-      `;
-    }
+    document.getElementById("again").addEventListener("click", function () {
+      document.write(`
+          hi again
+      `);
+      });
+  }
 }
-
-
