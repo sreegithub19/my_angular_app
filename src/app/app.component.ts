3ee192a8e8162fdbdf559e0f614a6851d9735a02
K <sreedhar.k@rakuten.com>
2023-04-08 09:19:55 +0530

    C

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7238,10 +7238,7 @@ function main(timestamp) {
       noteElement.innerHTML = \`Congrats, you did it!
         \${!hardMode ? "<p>Press H for hard mode</p>" : ""}
         <p>
-          Follow me
-          <a href="https://twitter.com/HunorBorbely" , target="_blank"
-            >@HunorBorbely</a
-          >
+          
         </p>\`;
       noteElement.style.opacity = 1;
       gameInProgress = false;
